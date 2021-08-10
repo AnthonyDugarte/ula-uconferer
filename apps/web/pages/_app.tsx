1e70f8a19c307afe13abada696a6398179f660b7
Anthony Dugarte <toonny1998@gmail.com>
2021-08-10 08:20:45 -0400

    feat: Add auth0 config

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,15 +1,21 @@
 import "../styles/globals.css";
+
 import type { AppProps } from "next/app";
 import { ApolloProvider } from "@apollo/client";
+import { UserProvider } from "@auth0/nextjs-auth0";
+
 import { clientBuilder } from "../clients";
 
 const client = clientBuilder(false);
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ApolloProvider client={client}>
-      <Component {...pageProps} />
-    </ApolloProvider>
+    <UserProvider>
+      <ApolloProvider client={client}>
+        <Component {...pageProps} />
+      </ApolloProvider>
+    </UserProvider>
   );
 }
+
 export default MyApp;
